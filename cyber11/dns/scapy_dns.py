c82bf3488a7d5ca670d2600a17806f7c89b85671
bouncingflopy <84018092+bouncingflopy@users.noreply.github.com>
2022-11-06 15:30:05 +0200

    Update scapy_dns.py

--- cyber11/dns/scapy_dns.py
+++ cyber11/dns/scapy_dns.py
@@ -1,18 +1,29 @@
 from scapy.all import *
 from scapy.layers.dns import DNS, DNSQR
-import socket
+from scapy.layers.inet import UDP, IP
 
-s = socket.socket()
-s.bind('0.0.0.0', 53)
 
-def dns_filter(packet):
-    return DNS in packet and packet[DNS].opcode == 0 and (packet[DNSQR].qtype == 1 or packet[DNSQR].qtype == 12)
+def load_data():
+    with open("database.txt", 'r+') as f:
+        lines = f.readlines()
+    return lines
+
+
+def dns_filter(p):
+    return DNS in p and p[DNS].opcode == 0 and (p[DNSQR].qtype == 1 or p[DNSQR].qtype == 12)
 
 
 def main():
+    d = [line.split(' ') for line in load_data()]
+
     while True:
-        p = sniff(count=1, lfilter=dns_filter)[0]
-        print(p[DNSQR].qname)
+        r = sniff(count=1, lfilter=dns_filter)[0]
+
+        if r[DNSQR].qtype == 1:
+            dnames = [d[i][1] for i in range(len(d)) if d[i][0] == "1"]
+            if r[DNSQR].qname.decode() in dnames:
+                p = IP(dst=r[IP].src, ttl=64)/UDP()/DNS(id=r[DNS].id, qr=1, ra=1)
+                print("need to answer " + r[DNSQR].qname.decode())
 
 
 if __name__ == "__main__":
